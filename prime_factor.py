cc6b019e5a982273612776041391f941204aa327
N07070 <nelson.perdriau@gmail.com>
2014-10-20 18:51:49 +0200

    Added a draft for the Prime Finder Problem.

---
+++ prime_factor.py
@@ -0,0 +1,31 @@
+"""Prime Factorization - Have the user
+enter a number and find all Prime Factors
+(if there are any) and display them."""
+
+#Check if the number is prime.
+def is_prime(n):
+	if n % 2 == 0:
+		return False
+
+	for i in range(3, int(n**0.5) + 1, 2):
+		if n % i == 0:
+			return False
+	return True
+
+input = int(raw_input("Enter a number >> "))
+factors = [1]
+#Two is always prime, so treat it on the side.
+if input % 2 == 0:
+	factors.append(2)
+
+#Check numbers from 3 and check if it's a interger.
+for x in range(3, int(input / 2) + 1):
+	if input % x == 0:
+		if is_prime(x) == True:
+	 			factors.append(x)
+
+if factors > 1:
+    print("This number is not Prime.\n")
+else:
+    print("This number is Fucking Prime !\n")
+print factors

125db6da539ff92878b28f1fdae8e6b842c42536
N07070 <nelson.perdriau@gmail.com>
2014-10-20 17:52:21 +0200

    Added the solution for finding Pi to the Nth Digit.

---
+++ pi_to_the_nth_digit.py
@@ -0,0 +1,17 @@
+""""Find PI to the Nth Digit"""
+
+#Import the math module
+import math
+
+#Ask until where Pi should go
+precision = int(raw_input("How far would you like to discover Pi ? >>"))
+
+#Define limit.
+while precision > 100:
+	print "Sorry, but you're going too far..."
+	precision = int(raw_input("How far would you like to discover Pi ? >>"))
+else:
+    #This prints Pi passed as an argument of %.
+    #*F give the Floating decimal format and precision is it's argument.
+    #See https://docs.python.org/2/library/stdtypes.html#string-formatting .
+	print '%.*F' % (precision, math.pi)

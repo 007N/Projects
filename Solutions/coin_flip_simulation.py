2c77e656f13c05a43ec0b60f9bd15731dd3bb4d5
N07070 <nelson.perdriau@gmail.com>
2014-10-27 16:48:57 +0100

    Added the solution for the coin flip simulation and updated the README.md

---
+++ Solutions/coin_flip_simulation.py
@@ -0,0 +1,21 @@
+"""Coin Flip Simulation - Write some code that simulates flipping a single coin however many times the user decides. The code should record the outcomes and count the number of tails and heads."""
+
+import random
+
+coin_flip = random.randint(0,1)
+nbr_user_flips = int(input("Please enter the number of times you want the coin to flip. >> "))
+nbr_flips_total = 0
+nbr_tails = 0
+nbr_heads = 0
+
+while nbr_flips_total < nbr_user_flips:
+    if random.randint(0,1) == 1:
+        print("\nThis coin gave tails.")
+        nbr_tails += 1
+        nbr_flips_total +=1
+    elif random.randint(0,1) == 0:
+        print("\nThis coin gave heads.")
+        nbr_heads += 1
+        nbr_flips_total +=1
+
+print("\nThis turn, heads came out",nbr_heads," times and tails came out",nbr_tails," times.")

6d12db0aaed011eb231caea4d5aa02ec1754ca7a
N07070 <nelson.perdriau@gmail.com>
2014-10-27 17:38:39 +0100

    Added the draft for the graph from links solution.

--- Solutions/coin_flip_simulation.py
+++ Solutions/coin_flip_simulation.py
@@ -2,20 +2,22 @@
 
 import random
 
-coin_flip = random.randint(0,1)
-nbr_user_flips = int(input("Please enter the number of times you want the coin to flip. >> "))
+coin_flip = random.randint(0, 1)
+nbr_user_flips = int(
+    input("Please enter the number of times you want the coin to flip. >> "))
 nbr_flips_total = 0
 nbr_tails = 0
 nbr_heads = 0
 
 while nbr_flips_total < nbr_user_flips:
-    if random.randint(0,1) == 1:
+    if random.randint(0, 1) == 1:
         print("\nThis coin gave tails.")
         nbr_tails += 1
-        nbr_flips_total +=1
-    elif random.randint(0,1) == 0:
+        nbr_flips_total += 1
+    elif random.randint(0, 1) == 0:
         print("\nThis coin gave heads.")
         nbr_heads += 1
-        nbr_flips_total +=1
+        nbr_flips_total += 1
 
-print("\nThis turn, heads came out",nbr_heads," times and tails came out",nbr_tails," times.")
+print("\nThis turn, heads came out", nbr_heads,
+      " times and tails came out", nbr_tails, " times.")
